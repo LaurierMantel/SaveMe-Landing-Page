b3e12fba40ebd6de658a82f3ee3c78fd9ad8e33b
Laurier Mantel <lauriermantel@gmail.com>
2016-01-08 18:52:36 -0500

    uses bootstrap CDN

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module Nineoneonelandingpage
   class Application < Rails::Application
-	config.assets.paths << "#{Rails}/vendor/assets/fonts"
+	config.assets.paths << Rails.root.join("vendor","assets", "fonts")
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
